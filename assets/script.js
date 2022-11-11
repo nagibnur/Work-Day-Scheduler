f5bd794f96c3115055bf07c847b524022a2973e3
nagibnur <nagibnur@hotmail.com>
2022-11-11 17:21:54 +0000

    set interval, test work

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 const currentDay = document.querySelector('#currentDay')
 const textarea = document.querySelector('.description')
 
-const date = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(date);
+
+const interval = setInterval(() => {
+    const date = moment().format('MMMM Do YYYY, h:mm:ss a');
+    currentDay.textContent = date
+    }, 1000);
+    
 
