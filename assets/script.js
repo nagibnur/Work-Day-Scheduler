99dfe02dec3e1d86b20c554e32204e62243b34a4
nagibnur <nagibnur@hotmail.com>
2022-11-11 17:20:29 +0000

    got date from moment

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,6 @@
+const currentDay = document.querySelector('#currentDay')
+const textarea = document.querySelector('.description')
 
+const date = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(date);
 
